4db1faac0ed2d2dbb368e8ff33d0b2b4f6e8d1db
khedayatullah452-lgtm <khedayatullah452@gmail.com>
2026-01-20 02:52:25 +0500

    Task7

---
+++ Tyuiu.kkhalid.Sprint6.Task7.V27.Test/DataServiceTest.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Kkhalid.Sprint6.Task7.V27.Lib;
+using System.IO;
+
+namespace Tyuiu.Kkhalid.Sprint6.Task7.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMatrix()
+        {
+            DataService ds = new DataService();
+
+            // ایجاد فایل CSV تست
+            string testFile = "test_matrix.csv";
+            string csvContent = @"1;2;3;4
+5;-6;7;-8
+9;10;11;12
+13;-14;15;-16
+17;18;-19;20
+21;22;23;24";
+
+            File.WriteAllText(testFile, csvContent);
+
+            try
+            {
+                int[,] matrix = ds.GetMatrix(testFile);
+
+                // بررسی ابعاد
+                Assert.AreEqual(6, matrix.GetLength(0)); // سطرها
+                Assert.AreEqual(4, matrix.GetLength(1)); // ستون‌ها
+
+                // بررسی برخی مقادیر
+                Assert.AreEqual(1, matrix[0, 0]);
+                Assert.AreEqual(-6, matrix[1, 1]);
+                Assert.AreEqual(-19, matrix[4, 2]); // سطر 5، ستون 3
+                Assert.AreEqual(24, matrix[5, 3]);
+            }
+            finally
+            {
+                if (File.Exists(testFile))
+                    File.Delete(testFile);
+            }
+        }
+
+        [TestMethod]
+        public void ValidChangeNegativeInFifthRow()
+        {
+            DataService ds = new DataService();
+
+            // ماتریس تست
+            int[,] testMatrix = new int[6, 4]
+            {
+                { 1, 2, 3, 4 },
+                { 5, -6, 7, -8 },
+                { 9, 10, 11, 12 },
+                { 13, -14, 15, -16 },
+                { 17, 18, -19, 20 }, // سطر پنجم
+                { 21, 22, 23, 24 }
+            };
+
+            int[,] result = ds.ChangeNegativeInFifthRow(testMatrix);
+
+            // بررسی تغییرات در سطر پنجم
+            Assert.AreEqual(17, result[4, 0]); // مثبت، تغییر نکرد
+            Assert.AreEqual(18, result[4, 1]); // مثبت، تغییر نکرد
+            Assert.AreEqual(-1, result[4, 2]); // منفی، باید به -1 تغییر کند
+            Assert.AreEqual(20, result[4, 3]); // مثبت، تغییر نکرد
+
+            // بررسی عدم تغییر در سطرهای دیگر
+            Assert.AreEqual(-6, result[1, 1]); // سطر دوم، تغییر نکرد
+            Assert.AreEqual(-8, result[1, 3]); // سطر دوم، تغییر نکرد
+            Assert.AreEqual(-14, result[3, 1]); // سطر چهارم، تغییر نکرد
+            Assert.AreEqual(-16, result[3, 3]); // سطر چهارم، تغییر نکرد
+        }
+
+        [TestMethod]
+        public void ValidChangeNegativeInFifthRow_NoNegative()
+        {
+            DataService ds = new DataService();
+
+            // ماتریس بدون عدد منفی در سطر پنجم
+            int[,] testMatrix = new int[5, 3]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
+                { 10, 11, 12 },
+                { 13, 14, 15 } // سطر پنجم بدون اعداد منفی
+            };
+
+            int[,] result = ds.ChangeNegativeInFifthRow(testMatrix);
+
+            // باید بدون تغییر باشد
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Assert.AreEqual(testMatrix[i, j], result[i, j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ValidChangeNegativeInFifthRow_SmallMatrix()
+        {
+            DataService ds = new DataService();
+
+            // ماتریس کوچکتر از ۵ سطر
+            int[,] testMatrix = new int[3, 3]
+            {
+                { 1, -2, 3 },
+                { -4, 5, -6 },
+                { 7, -8, 9 }
+            };
+
+            int[,] result = ds.ChangeNegativeInFifthRow(testMatrix);
+
+            // باید بدون تغییر باشد (سطر پنجم وجود ندارد)
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Assert.AreEqual(testMatrix[i, j], result[i, j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ValidSaveToCsvFile()
+        {
+            DataService ds = new DataService();
+
+            // ماتریس تست
+            int[,] testMatrix = new int[3, 3]
+            {
+                { 1, 2, 3 },
+                { -4, 5, -6 },
+                { 7, 8, 9 }
+            };
+
+            string testFile = "test_output.csv";
+
+            try
+            {
+                // ذخیره فایل
+                ds.SaveToCsvFile(testMatrix, testFile);
+
+                // بررسی وجود فایل
+                Assert.IsTrue(File.Exists(testFile));
+
+                // خواندن و بررسی محتوا
+                string[] lines = File.ReadAllLines(testFile);
+                Assert.AreEqual(3, lines.Length);
+                Assert.AreEqual("1;2;3", lines[0]);
+                Assert.AreEqual("-4;5;-6", lines[1]);
+                Assert.AreEqual("7;8;9", lines[2]);
+            }
+            finally
+            {
+                if (File.Exists(testFile))
+                    File.Delete(testFile);
+            }
+        }
+    }
+}
---
+++ Tyuiu.kkhalid.Sprint6.Task7.V27/FormMain.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Tyuiu.Kkhalid.Sprint6.Task7.V27
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.groupBoxInput = new System.Windows.Forms.GroupBox();
+            this.dataGridViewIn = new System.Windows.Forms.DataGridView();
+            this.groupBoxOutput = new System.Windows.Forms.GroupBox();
+            this.dataGridViewOut = new System.Windows.Forms.DataGridView();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.openFileDialogTask = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogTask = new System.Windows.Forms.SaveFileDialog();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.groupBoxInput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIn)).BeginInit();
+            this.groupBoxOutput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOut)).BeginInit();
+            this.SuspendLayout();
+
+            // groupBoxInput
+            this.groupBoxInput.Controls.Add(this.dataGridViewIn);
+            this.groupBoxInput.Location = new System.Drawing.Point(12, 50);
+            this.groupBoxInput.Name = "groupBoxInput";
+            this.groupBoxInput.Size = new System.Drawing.Size(400, 400);
+            this.groupBoxInput.TabIndex = 0;
+            this.groupBoxInput.TabStop = false;
+            this.groupBoxInput.Text = "Входная матрица";
+
+            // dataGridViewIn
+            this.dataGridViewIn.AllowUserToAddRows = false;
+            this.dataGridViewIn.AllowUserToDeleteRows = false;
+            this.dataGridViewIn.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewIn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewIn.Location = new System.Drawing.Point(3, 16);
+            this.dataGridViewIn.Name = "dataGridViewIn";
+            this.dataGridViewIn.ReadOnly = true;
+            this.dataGridViewIn.Size = new System.Drawing.Size(394, 381);
+            this.dataGridViewIn.TabIndex = 0;
+
+            // groupBoxOutput
+            this.groupBoxOutput.Controls.Add(this.dataGridViewOut);
+            this.groupBoxOutput.Location = new System.Drawing.Point(428, 50);
+            this.groupBoxOutput.Name = "groupBoxOutput";
+            this.groupBoxOutput.Size = new System.Drawing.Size(400, 400);
+            this.groupBoxOutput.TabIndex = 1;
+            this.groupBoxOutput.TabStop = false;
+            this.groupBoxOutput.Text = "Результат";
+
+            // dataGridViewOut
+            this.dataGridViewOut.AllowUserToAddRows = false;
+            this.dataGridViewOut.AllowUserToDeleteRows = false;
+            this.dataGridViewOut.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewOut.Location = new System.Drawing.Point(3, 16);
+            this.dataGridViewOut.Name = "dataGridViewOut";
+            this.dataGridViewOut.ReadOnly = true;
+            this.dataGridViewOut.Size = new System.Drawing.Size(394, 381);
+            this.dataGridViewOut.TabIndex = 0;
+
+            // buttonOpenFile
+            this.buttonOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(120, 30);
+            this.buttonOpenFile.TabIndex = 2;
+            this.buttonOpenFile.Text = "Открыть файл";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+
+            // buttonProcess
+            this.buttonProcess.Enabled = false;
+            this.buttonProcess.Location = new System.Drawing.Point(138, 12);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(120, 30);
+            this.buttonProcess.TabIndex = 3;
+            this.buttonProcess.Text = "Обработать";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+
+            // buttonSave
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(264, 12);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(120, 30);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+
+            // buttonInfo
+            this.buttonInfo.Location = new System.Drawing.Point(708, 12);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(120, 30);
+            this.buttonInfo.TabIndex = 5;
+            this.buttonInfo.Text = "Справка";
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            this.buttonInfo.Click += new System.EventHandler(this.buttonInfo_Click);
+
+            // openFileDialogTask
+            this.openFileDialogTask.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            this.openFileDialogTask.Title = "Выберите CSV файл";
+
+            // saveFileDialogTask
+            this.saveFileDialogTask.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            this.saveFileDialogTask.Title = "Сохранить результат";
+
+            // labelStatus
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(12, 460);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(138, 13);
+            this.labelStatus.TabIndex = 6;
+            this.labelStatus.Text = "Готов к работе...";
+
+            // FormMain
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 480);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.groupBoxOutput);
+            this.Controls.Add(this.groupBoxInput);
+            this.MinimumSize = new System.Drawing.Size(856, 519);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Спринт 6 | Таск 7 | Вариант 27 | Халид К.К.";
+            this.groupBoxInput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewIn)).EndInit();
+            this.groupBoxOutput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOut)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.GroupBox groupBoxInput;
+        private System.Windows.Forms.DataGridView dataGridViewIn;
+        private System.Windows.Forms.GroupBox groupBoxOutput;
+        private System.Windows.Forms.DataGridView dataGridViewOut;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonInfo;
+        private System.Windows.Forms.OpenFileDialog openFileDialogTask;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogTask;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
